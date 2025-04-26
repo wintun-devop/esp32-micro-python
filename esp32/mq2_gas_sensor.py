c5b64ee575067fcb3a6409375938bb3a477607f9
wintun-devop <wintun.hlaing@outlook.com>
2025-04-26 17:51:23 +0630

    mq2_sensor

--- esp32/mq2_gas_sensor.py
+++ esp32/mq2_gas_sensor.py
@@ -5,22 +5,20 @@ from time import sleep
 #build in led for operation status
 led = Pin(2, Pin.OUT)
 #Digital output on GPIO15
-do_pin = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_DOWN)
+do_pin = machine.Pin(15, machine.Pin.IN)
 
 def main():
     while True:
         gas_detected = do_pin.value()
         #led on
         led.value(1)
-        print("value",gas_detected)
-        print("pin",do_pin)
         if gas_detected:
             print("⚠ High gas concentration detected!")
         else:
             print("✅ Air quality is normal.")
         #led off
         led.value(0)
-        sleep(2)
+        sleep(1)
 
 if __name__ == "__main__":
     main()
